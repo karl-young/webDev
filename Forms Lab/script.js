689a6966ed09b806ceaba03aaaaf8daaebf98a4c
Karl Young (08005603) <YOUNGKG1@student.op.ac.nz>
2025-08-22 11:53:03 +1200

    Working through Form

---
+++ Forms Lab/script.js
@@ -0,0 +1,14 @@
+let answers = {
+     q1: "Central Processing Unit",
+     q2: "q2-memory" ,
+     q3: ["CPU" , "CentralPU"],
+     q4: "Harddrive" ,
+     q5: "InternetProtocol" ,
+    }
+
+function GetData(){
+
+    let q1Data = document.getElementsByName("question1").value()
+
+    
+}
