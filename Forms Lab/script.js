0e00b7bcb72573dd48dcbfb416993ed92f94c81b
unknown <YOUNGKG1@student.op.ac.nz>
2025-08-24 21:09:49 +1200

    q1 works no idea for q2

--- Forms Lab/script.js
+++ Forms Lab/script.js
@@ -1,14 +1,49 @@
 let answers = {
-     q1: "Central Processing Unit",
-     q2: "q2-memory" ,
-     q3: ["CPU" , "CentralPU"],
-     q4: "Harddrive" ,
-     q5: "InternetProtocol" ,
+  q1: ['central processing unit', 'cpu'],
+  q2: 'memory',
+  q3: ['cpu', 'centralpu'],
+  q4: 'Harddrive',
+  q5: 'InternetProtocol',
+}
+const btn = document.getElementById('submit-btn')
+const output = document.getElementById('output')
+let correctAnswer = false
+let correctAnswer1 = false
+
+function CheckAnswers() {
+  try {
+    let question1 = document
+      .getElementsByName('question1')[0]
+      .value.trim()
+      .toLowerCase()
+
+    if (answers.q1.includes(question1)) {
+      correctAnswer = true
+    } else {
+      correctAnswer = false
+    }
+    output.textContent = `You Said1 = ${question1}, ${correctAnswer}`
+
+    let question2 = document.getElementsByName('q2')
+
+    for (let i = 0; i < question2.length; i++) {
+      if (question2.checked === true) {
+        let data = question2.checked.value.trim().toLowerCase()
+        if (data == answers.q2) {
+          correctAnswer1 = true
+        } else {
+          correctAnswer1 = false
+        }
+      }
     }
 
-function GetData(){
-
-    let q1Data = document.getElementsByName("question1").value()
-
-    
+    output.textContent = `You Said2 = ${question2}, ${correctAnswer1}`
+  } catch (e) {
+    console.log('An error occurred getting data!')
+  }
 }
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+  CheckAnswers()
+})
